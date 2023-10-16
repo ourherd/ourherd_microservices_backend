4604b76861170a046935a81a4b0b30eb1a2d1d2f
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-16 15:50:37 +1100

    Add rabbit service to story
    Create story queue

--- apps/story/src/main.ts
+++ apps/story/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { StoryModule } from './story.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { RABBIT_SERVICE_OPTIONS } from '@app/rabbit';
+import { STORY_MODULE, STORY_SERVICE } from "./constant/story-patterns.constants";
+
 
 async function bootstrap() {
+
+  let logger = new Logger(STORY_MODULE);
+
   const app = await NestFactory.create(StoryModule);
-  await app.listen(3000);
+  // * setup
+  app.connectMicroservice<MicroserviceOptions>(app.get<MicroserviceOptions>(RABBIT_SERVICE_OPTIONS));
+  // * start
+  await app.startAllMicroservices();
+
+  logger.log(
+    `🚀 { ` + STORY_SERVICE +` } running on port ` ,
+  );
+
 }
 bootstrap();
--- apps/story/src/story.module.ts
+++ apps/story/src/story.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { StoryController } from './story.controller';
 import { StoryService } from './story.service';
+import { RabbitModule, RabbitServiceName } from '@app/rabbit';
+// import { MemberEntity } from './entity/member.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Database, DatabaseModule } from '@app/database';
+import { getEnvPath } from '@app/common/env/env.helper';
+import { MemberEntity } from "../../member/src/entity/member.entity";
+import { MemberController } from "../../member/src/member.controller";
+import { MemberService } from "../../member/src/member.service";
+import { IsEmailUserAlreadyExistConstraint } from "@app/common/validation-rules/email-not-registered.rule";
+
+const envFilePath: string = getEnvPath(`${__dirname}/`);
+console.log(envFilePath)
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: envFilePath
+    }),
+
+    DatabaseModule.register(Database.PRIMARY),
+    DatabaseModule.forEntity(Database.PRIMARY, [MemberEntity]),
+    RabbitModule.forServerProxy(RabbitServiceName.STORY)
+  ],
   controllers: [StoryController],
   providers: [StoryService],
 })
+
 export class StoryModule {}
