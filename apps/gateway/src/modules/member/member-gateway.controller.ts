5cad9105ce9ab3cf73647abe5c230077952a4e0c
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-16 20:50:35 +1100

    Update members list

--- apps/gateway/src/modules/member/member-gateway.controller.ts
+++ apps/gateway/src/modules/member/member-gateway.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Query } from "@nestjs/common";
 import { IGatewayResponse } from '../../common/interface/gateway.interface';
+import { IServiceResponse, RabbitServiceName } from "@app/rabbit";
+import { IPagination, PaginationDto } from '@app/common';
 import { MemberEntity } from 'apps/member/src/entity/member.entity';
+import { FindMemberDto } from "apps/member/src/dto/find.member.dto";
 import { Auth, CurrentUser } from "@app/authentication";
 import { ApiTags } from "@nestjs/swagger";
+import { firstValueFrom } from "rxjs";
+import { MEMBER_MESSAGE_PATTERNS } from "../../../../member/src/constant/member-patterns.constants";
+import { ClientProxy } from "@nestjs/microservices";
 
 @ApiTags('User Gateway')
 @Controller({
@@ -12,6 +18,10 @@ import { ApiTags } from "@nestjs/swagger";
 // @Auth()
 export class MemberGatewayController {
 
+  constructor(
+    @Inject(RabbitServiceName.MEMBER) private memberClient: ClientProxy)
+  { }
+
   @Get('/me')
   async getSelf() {
     return {
@@ -19,4 +29,20 @@ export class MemberGatewayController {
       data: 'member'
     };
   }
+
+  @Get('/')
+  async getMembers ( @Query() findDto: FindMemberDto ): Promise<IGatewayResponse<IPagination<MemberEntity>>> {
+    const { state, data } = await firstValueFrom(
+      this.memberClient.send<IServiceResponse<IPagination<MemberEntity>>>
+      (
+        MEMBER_MESSAGE_PATTERNS.FIND_ALL,
+        findDto
+      )
+    );
+    return { state, data };
+  }
+
+
+
+
 }
--- apps/gateway/src/modules/member/member-profile-gateway.controller.ts
+++ apps/gateway/src/modules/member/member-profile-gateway.controller.ts
@@ -55,5 +55,4 @@ export class MemberProfileGatewayController {
     return { state, data };
   }
 
-
 }
