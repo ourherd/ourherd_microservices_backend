7eef3e1dd7debd53a4030d42115ab669376618ec
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-16 15:41:32 +1100

    Change env variables

--- apps/member/src/member.module.ts
+++ apps/member/src/member.module.ts
@@ -6,12 +6,17 @@ import { MemberEntity } from './entity/member.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Database, DatabaseModule } from '@app/database';
 import { IsEmailUserAlreadyExistConstraint } from "@app/common/validation-rules/email-not-registered.rule";
+import { getEnvPath } from '@app/common/env/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/`);
+console.log(envFilePath)
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: './.env'
+      envFilePath: envFilePath
     }),
+
     DatabaseModule.register(Database.PRIMARY),
     DatabaseModule.forEntity(Database.PRIMARY, [MemberEntity]),
     RabbitModule.forServerProxy(RabbitServiceName.MEMBER)
