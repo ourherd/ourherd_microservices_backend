3682e284e515d01103904bbfb9f658ba44d0ff55
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-18 18:53:34 +1100

    Change member controller

--- apps/gateway/src/modules/member/member-profile-gateway.controller.ts
+++ apps/gateway/src/modules/member/member-profile-gateway.controller.ts
@@ -1,15 +1,6 @@
-import { Auth, CurrentUser } from '@app/authentication';
-import { IServiceResponse, RabbitServiceName } from '@app/rabbit';
-import { uuid } from 'uuidv4';
-import { Body, Controller, Inject, Patch, Post, Param, ParseUUIDPipe, Logger } from "@nestjs/common";
-import { ClientProxy } from '@nestjs/microservices';
-import { IGatewayResponse } from '../../common/interface/gateway.interface';
-import { MemberEntity } from 'apps/member/src/entity/member.entity';
-import { UpdateMemberDto } from "apps/member/src/dto/update-member.dto";
-import { CreateMemberDto } from "apps/member/src/dto/create-member.dto";
-import { MEMBER_MESSAGE_PATTERNS, MEMBER_SERVICE } from "apps/member/src/constant/member-patterns.constants";
-import { firstValueFrom } from 'rxjs';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Logger } from "@nestjs/common";
+import { MEMBER_SERVICE } from "apps/member/src/constant/member-patterns.constants";
+import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags('Profile Gateway')
 @Controller({
@@ -20,42 +11,12 @@ export class MemberProfileGatewayController {
 
   logger = new Logger(MEMBER_SERVICE);
 
-  constructor( @Inject(RabbitServiceName.MEMBER) private memberService: ClientProxy ) { }
-
-  @Post('/')
-  async createProfile (
-    @Body() createDto: CreateMemberDto,
-  ) : Promise<IGatewayResponse> {
-
-    const { state, data } = await firstValueFrom(
-      this.memberService.send<IServiceResponse<MemberEntity>, { createDto: CreateMemberDto }>
-      (
-        MEMBER_MESSAGE_PATTERNS.CREATE,
-        {
-          createDto
-        }
-      )
-    );
-    return { state, data };
-  };
-
-  @Patch('/:id')
-  async updateProfile(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() updateDto: UpdateMemberDto
-  ) : Promise<IGatewayResponse> {
-
-    const { state, data } = await firstValueFrom(
-      this.memberService.send<IServiceResponse<MemberEntity>, { id: string, updateDto: UpdateMemberDto }>(
-        MEMBER_MESSAGE_PATTERNS.UPDATE,
-        {
-          id,
-          updateDto
-        }
-      )
-    );
-
-    return { state, data };
+  @Get('/me')
+  async getProfile() {
+    return {
+      state: true,
+      data: 'profile'
+    };
   }
 
 }
