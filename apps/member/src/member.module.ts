2960e211daf861fbdc509ad0b97cfd14f754c950
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-23 17:05:30 +1100

    Disable injection email validation

--- apps/member/src/member.module.ts
+++ apps/member/src/member.module.ts
@@ -1,12 +1,11 @@
-import { Module } from '@nestjs/common';
-import { MemberController } from './member.controller';
-import { MemberService } from './member.service';
-import { RabbitModule, RabbitServiceName } from '@app/rabbit';
-import { MemberEntity } from './entity/member.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Database, DatabaseModule } from '@app/database';
-import { getEnvPath } from '@app/common/env/env.helper';
-import { IsEmailNotRegistered } from "@app/common/validation-rules/email-not-registered.rule";
+import { Module } from "@nestjs/common";
+import { MemberController } from "./member.controller";
+import { MemberService } from "./member.service";
+import { RabbitModule, RabbitServiceName } from "@app/rabbit";
+import { MemberEntity } from "./entity/member.entity";
+import { ConfigModule } from "@nestjs/config";
+import { Database, DatabaseModule } from "@app/database";
+import { getEnvPath } from "@app/common/env/env.helper";
 
 const envFilePath: string = getEnvPath(`${__dirname}/`);
 
@@ -21,7 +20,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/`);
     RabbitModule.forServerProxy(RabbitServiceName.MEMBER)
   ],
   controllers: [MemberController],
-  providers: [IsEmailNotRegistered, MemberService],
+  providers: [MemberService],
 })
 
 export class MemberModule {}
