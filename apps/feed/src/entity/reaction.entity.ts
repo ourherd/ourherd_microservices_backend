43fb3d6722b94444b4467cbb72ff0032f7652098
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-24 19:44:06 +1100

    Add swagger to reaction

--- apps/feed/src/entity/reaction.entity.ts
+++ apps/feed/src/entity/reaction.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 import { AbstractEntity } from "@app/database/base/base.entity";
 
 export enum ReactionType {
@@ -15,12 +16,18 @@ export enum ReactionType {
 })
 
 export class ReactionEntity extends AbstractEntity {
+
+  @ApiProperty()
   @Column({ nullable: false })
   member_id: string;
 
+  @ApiProperty()
   @Column({ nullable: false })
   story_id: string;
 
+  @ApiProperty({
+    enum: ReactionType
+  })
   @Column({
     type: "enum",
     enum: ReactionType,
