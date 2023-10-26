97a1b013044e13bc606124098a79c72586f6759c
jokekerker <jokekerker@gmail.com>
2023-10-26 14:21:08 +1000

    [REF] add password gateway into module

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -14,6 +14,7 @@ import { PassportModule } from '@nestjs/passport';
 import { FeedGatewayController } from './modules/feed/feed-gateway.controller';
 import { ReactionGatewayController } from './modules/reaction/reaction-gateway.controller';
 import { AccountGatewayController } from "./modules/account/account-gateway.controller";
+import { AccountPasswordGatewayController } from "./modules/account/account-password-gateway.controller";
 const envFilePath: string = getEnvPath(`${__dirname}/`);
 
 @Module({
@@ -41,6 +42,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/`);
   ],
   controllers: [
     AccountGatewayController,
+    AccountPasswordGatewayController,
     MemberGatewayController,
     MemberProfileGatewayController,
     FeedGatewayController,
