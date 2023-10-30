c478fcf98e485f7a7a4f443f3118c365bcd20867
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-30 18:12:38 +1100

    Update enpoint path

--- apps/story/src/dto/story.draft.video.dto.ts
+++ apps/story/src/dto/story.draft.video.dto.ts
@@ -1,8 +1,19 @@
-import { IsString, IsUUID } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { StoryDraftBaseDto } from "./story.draft.base.dto";
+import { Transform } from "class-transformer";
+import { StoryMedium, StoryType } from "../constant/story.enum";
 
 export class StoryDraftVideoDto extends StoryDraftBaseDto {
 
+  @Transform(({ value }) => value = StoryType.VIDEO )
+  @IsString()
+  @IsOptional()
+  @IsEnum(StoryType)
+  readonly story_type: StoryType = StoryType.VIDEO;
+
+  @IsString()
+  @IsOptional()
+  @IsEnum(StoryMedium)
+  readonly story_medium: StoryMedium = StoryMedium.VIDEO;
 
 }
