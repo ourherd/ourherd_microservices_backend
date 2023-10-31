308fd19ee580758ec3ef9dbb0921a2dc259b30e9
Ronald Sandoval <ron.sandoval@gmail.com>
2023-10-31 15:14:10 +1100

    Change constants for video and image format

--- libs/common/src/constant/upload.constant.ts
+++ libs/common/src/constant/upload.constant.ts
@@ -1,4 +1,5 @@
-export const MAX_UPLOAD_FILE_SIZE = 1024 * 1000 * 5;
-export const AVAILABLE_UPLOAD_FILE_MIMES = ['jpg', 'jpeg', 'png'];
+export const MAX_UPLOAD_IMAGE_FILE_SIZE = 1024 * 1000 * 10;
 export const AVAILABLE_UPLOAD_IMAGES_FILE_MIMES = ['jpg', 'jpeg', 'png'];
-export const AVAILABLE_UPLOAD_VIDEO_FILE_MIMES = ['avi', 'mp4', 'vid'];
+
+export const MAX_UPLOAD_FILE_VIDEO_SIZE = 1024 * 1000 * 150;
+export const AVAILABLE_UPLOAD_VIDEO_FILE_MIMES = ['avi', 'mp4', 'vid', 'mov'];
