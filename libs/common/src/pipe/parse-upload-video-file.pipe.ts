571e02b6bee5e7d3eacf3d8ba7c68f239abe1662
jokekerker <jokekerker@gmail.com>
2023-11-07 11:07:14 +1000

    [REF] fix incorrect extends class constrcuture

--- libs/common/src/pipe/parse-upload-video-file.pipe.ts
+++ libs/common/src/pipe/parse-upload-video-file.pipe.ts
@@ -4,11 +4,7 @@ import { ParseUploadFilePipe } from "@app/common/pipe/parse-upload-file-pipe";
 
 @Injectable()
 export class ParseUploadVideoFilePipe extends ParseUploadFilePipe {
-    constructor(
-        private required: boolean = true,
-        private mimes: string[] = AVAILABLE_UPLOAD_VIDEO_FILE_MIMES,
-        private maxSize: number = MAX_UPLOAD_FILE_VIDEO_SIZE
-    ) {
+    constructor() {
         super(true , AVAILABLE_UPLOAD_VIDEO_FILE_MIMES , MAX_UPLOAD_FILE_VIDEO_SIZE);
     }
 
