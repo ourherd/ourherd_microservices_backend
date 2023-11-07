0ad28cb3c44c5c8bd8cc90ff700f4e031ddb0979
Ronald Sandoval <ron.sandoval@gmail.com>
2023-11-07 13:54:35 +1100

    Fixed conflict

--- libs/database/src/constant/database.constant.ts
+++ libs/database/src/constant/database.constant.ts
@@ -7,11 +7,8 @@ import { AccountDeviceEntity } from 'apps/account/src/entity/account.device.enti
 import { ReactionEntity } from 'apps/feed/src/entity/reaction.entity';
 import { StoryEntity } from 'apps/story/src/entity/story.entity';
 import { StoryBookmarkEntity } from 'apps/story/src/entity/story.bookmark.entity';
-<<<<<<< HEAD
 import { StorageResourceEntity } from "../../../../apps/storage/src/entity/storage-resource.entity";
-=======
 import { AccountVerificationEntity } from 'apps/account/src/entity/email-verification.entity';
->>>>>>> develop
 // import { AccountEntity } from 'apps/account/src/entity/account.entity';
 // import { AuthRequestEntity } from "apps/auth/src/entity/auth-request.entity";
 
@@ -20,46 +17,28 @@ export const DATABASE_CONFIG: Record<Database, { type: DatabaseType, env: string
         type: 'postgres',
         env: 'PRIMARY',
         entities: [
-<<<<<<< HEAD
-          MemberEntity,
-          ReactionEntity,
-          StoryEntity,
-          StoryBookmarkEntity,
-          AccountEntity,
-          AccountDeviceEntity,
-          StorageResourceEntity
-=======
             MemberEntity,
             ReactionEntity,
             StoryEntity,
             StoryBookmarkEntity,
             AccountEntity,
             AccountDeviceEntity,
+            StorageResourceEntity,
             AccountVerificationEntity
->>>>>>> develop
         ]
     },
     secondary: {
         type: 'postgres',
         env: 'SECONDARY',
         entities: [
-<<<<<<< HEAD
           MemberEntity,
           ReactionEntity,
           StoryEntity,
           StoryBookmarkEntity,
           AccountEntity,
           AccountDeviceEntity,
-          StorageResourceEntity
-=======
-            MemberEntity,
-            ReactionEntity,
-            StoryEntity,
-            StoryBookmarkEntity,
-            AccountEntity,
-            AccountDeviceEntity,
-            AccountVerificationEntity
->>>>>>> develop
+          StorageResourceEntity,
+          AccountVerificationEntity
         ]
     }
 };
