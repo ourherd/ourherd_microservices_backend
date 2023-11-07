53569b14122ad5c47a07a5e20315d091fa05f6b1
Ronald Sandoval <ron.sandoval@gmail.com>
2023-11-08 00:05:48 +1100

    Remove update on restrict sql

--- docs/db/story_violation.sql
+++ docs/db/story_violation.sql
@@ -2,12 +2,15 @@ create table public.story_violations
 (
     id  uuid default gen_random_uuid() not null constraint story_violation_pkey primary key,
     reason text,
-    created_at timestamp with time zone default now() not null,
 
     story_id uuid not null constraint story_violation_story_id_fkey
-            references stories on update restrict on delete restrict,
+            references stories on delete restrict,
 
     member_id uuid not null constraint story_violation_user_id_fkey
-            references "members" on update restrict on delete restrict
+            references "members" on delete restrict,
 
+    created_at    timestamp with time zone default now() not null,
+    updated_at    timestamp with time zone default now() not null,
+    deleted_at timestamptz,
+    constraint unique_member_id_story_id_violation unique (member_id, story_id)
 );
