811a8549e7e4ad38d63595558e43e9f9e30324d6
Ronald Sandoval <ron.sandoval@gmail.com>
2023-11-08 21:30:59 +1100

    Add class validator

--- apps/member/src/dto/verify-email.member.dto.ts
+++ apps/member/src/dto/verify-email.member.dto.ts
@@ -1,18 +1,21 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString } from "class-validator";
 
 export class VerifyUserDto {
-  @IsEmail()
+
   @ApiProperty({
     description: "user email for matching account information",
     example: "info@batyr.com.au"
   })
+  @IsEmail()
   email: string;
 
-  @IsString()
+
   @ApiProperty({
     description: "for receiving token code from verify link",
     example: "b10aee82-5393-4517-8025-6deb707fa453"
   })
+  @IsString()
   confirmationCode: string;
+
 }
