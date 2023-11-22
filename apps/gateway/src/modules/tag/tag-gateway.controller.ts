f5a3833bd986cf9d1e78fffc3be9dd7a6bdc1ee1
Ronald Sandoval <ron.sandoval@gmail.com>
2023-11-22 15:25:36 +1100

    Add gateway tags

--- apps/gateway/src/modules/tag/tag-gateway.controller.ts
+++ apps/gateway/src/modules/tag/tag-gateway.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Post, Query } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { IServiceResponse, RabbitServiceName } from "@app/rabbit";
 import { firstValueFrom } from "rxjs";
@@ -9,6 +9,11 @@ import { TagEntity } from "../../../../tag/src/entity/tag.entity";
 import { IGatewayResponse } from "../../common/interface/gateway.interface";
 import { Auth } from "@app/authentication";
 import { TAG_MESSAGE_PATTERNS } from "../../../../tag/src/constant/tag-patterns.constants";
+import { FindMemberDto } from "../../../../member/src/dto/find.member.dto";
+import { IPagination } from "@app/common";
+import { MemberEntity } from "../../../../member/src/entity/member.entity";
+import { MEMBER_MESSAGE_PATTERNS } from "../../../../member/src/constant/member-patterns.constants";
+import { AllTagsAppDto } from "../../../../tag/src/dto/all.tags.app.dto";
 
 @ApiTags('Tags Gateway')
 @Controller({
@@ -17,6 +22,22 @@ import { TAG_MESSAGE_PATTERNS } from "../../../../tag/src/constant/tag-patterns.
 export class TagGatewayController {
 
   constructor(@Inject(RabbitServiceName.TAG) private tagClient: ClientProxy) { }
+
+
+  @Auth()
+  @Get('/all')
+  // async getMembers ( @Query() findDto: FindMemberDto ): Promise<IGatewayResponse<IPagination<MemberEntity>>> {
+  async getTagsApp ( @Query() findDto: AllTagsAppDto ): Promise<IGatewayResponse<IPagination<TagEntity>>> {
+    const { state, data } = await firstValueFrom(
+      this.tagClient.send<IServiceResponse<IPagination<TagEntity>>>
+      (
+        TAG_MESSAGE_PATTERNS.ALL,
+        findDto
+      )
+    );
+    return { state, data };
+  }
+
 
   @Post('/')
   @Auth()
