1199b1f981a6e1f81487112651a78b9256b4b615
Ronald Sandoval <ron.sandoval@gmail.com>
2023-11-29 14:29:07 +1100

    Remove unused code

--- apps/storage/src/service/driver/resource-s3-aws-service.ts
+++ apps/storage/src/service/driver/resource-s3-aws-service.ts
@@ -75,14 +75,4 @@ export class ResourceS3AwsService {
     }
   }
 
-  // TODO add stream
-  // private async stream_buffer(stream: Readable): Promise<Buffer> {
-  //   const chunks = []
-  //   return new Promise((resolve, reject) => {
-  //     stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)))
-  //     stream.on('error', (err) => reject(err))
-  //     stream.on('end', () => resolve(Buffer.concat(chunks)))
-  //   })
-  // }
-
 }
