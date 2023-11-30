2f011b642b4764e32f663e50f2b648ddf5ca3896
ronsandova <ron.sandoval@gmail.com>
2023-11-30 15:30:55 +1100

    OH2-272 untitled story (#46)
    * Add default value for title story when its draft
    
    Fix day from string to number
    
    * Change message format

--- apps/story/src/constant/story-patterns.constants.ts
+++ apps/story/src/constant/story-patterns.constants.ts
@@ -1,22 +1,19 @@
 export const STORY_SERVICE = 'STORY SERVICE';
 export const STORY_MODULE = 'STORY MODULE';
+export const STORY_DEFAULT_TITLE = 'Untitled';
 
 export const STORY_MESSAGE_PATTERNS = {
-
   DRAFT_TEXT_FREE_FORM: 'story.draft-text-freeform',
   DRAFT_TEXT_GUIDE: 'story.draft-text-guide',
   DRAFT_VIDEO: 'story.draft-video',
-
   UPDATE_TEXT_FREE_FORM: 'story.update-text-freeform',
   UPDATE_TEXT_GUIDE: 'story.update-text-guide',
   UPDATE_VIDEO: 'story.update-video',
   UPDATE_SETTING: 'story.update-setting',
-
   UPDATE_MEDIA_IMAGE_FREE_FORM: 'story.update-meida-image-free-form',
   UPDATE_MEDIA_IMAGE_GUIDE: 'story.update-meida-image-guide',
   UPDATE_MEDIA_VIDEO: 'story.update-meida-video',
   FIND_BY_ID: 'story.findbyid',
-
   SUBMIT_STORY: 'story.submit-story'
 }
 
@@ -33,3 +30,6 @@ export const STORY_MESSAGE_DB_RESPONSE = {
   NO_FOUND : 'story.no-found'
 }
 
+export const STORY_MESSAGE = {
+  MIN_AGE : 'You need to 13 years old or older to create an account.'
+}
