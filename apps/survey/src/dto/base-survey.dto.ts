9801c6dda93a376a2996f21e9f08c6c877d9a4ed
Ronald Sandoval <ron.sandoval@gmail.com>
2023-12-07 11:54:04 +1100

    Allow ids (uuid) from the front end

--- apps/survey/src/dto/base-survey.dto.ts
+++ apps/survey/src/dto/base-survey.dto.ts
@@ -1,14 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsUUID } from "class-validator";
-import { Transform } from "class-transformer";
-import { v4 } from "uuid";
 
 export abstract class AbstractSurveyDto {
 
   @ApiProperty()
   @IsUUID()
-  @Transform(({ value }) => value = v4())
-  public id: string = v4();
+  @IsOptional()
+  public id: string;
 
   @ApiProperty()
   @IsOptional()
